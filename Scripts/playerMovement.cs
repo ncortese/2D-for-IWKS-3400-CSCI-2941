25ad8ef5f9db3b1ce7d09a532a3fab13c99e5880
mibikin <mibikin@gmail.com>
2018-02-11 21:16:48 -0700

    Add player movement code and player prefab
    The player movement code has been added and can be controlled from within unity to an extent. Needs to be tested for if you guys like the feel but I think its fine for now at least. Temporary sprite is included as well.

---
+++ Scripts/playerMovement.cs
@@ -0,0 +1,93 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class playerMovement : MonoBehaviour {
+
+    private enum Direction { north = 0, south = 1, east = 2, west = 3 }
+
+    // Public values can be adjusted in unity
+    public int speed = 4;
+    public int movementCooldown = 30;
+    public bool allowHoldToMove = true;
+
+    // Private values for behind the scenes work
+    private int movementTimer = 0;
+    private Direction direction;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (allowHoldToMove) {
+            ControlPlayerHold();
+        }
+        else {
+            ControlPlayerTap();
+        }
+        UpdateTimer();
+	}
+
+    void ControlPlayerHold() {
+        // Used for moving the player
+        Vector3 movePlayer = Vector3.zero;
+
+        if (Input.GetKey(KeyCode.LeftArrow) && movementTimer == 0) {
+            direction = Direction.west;
+            movePlayer.x = -speed;
+            movementTimer += movementCooldown;
+        }
+        if (Input.GetKey(KeyCode.RightArrow) && movementTimer == 0) {
+            direction = Direction.east;
+            movePlayer.x = speed;
+            movementTimer += movementCooldown;
+        }
+        if (Input.GetKey(KeyCode.UpArrow) && movementTimer == 0) {
+            direction = Direction.north;
+            movePlayer.y = speed;
+            movementTimer += movementCooldown;
+        }
+        if (Input.GetKey(KeyCode.DownArrow) && movementTimer == 0) {
+            direction = Direction.south;
+            movePlayer.y = -speed;
+            movementTimer += movementCooldown;
+        }
+
+        transform.position += movePlayer;
+    }
+    void ControlPlayerTap() {
+        // Used for moving the player
+        Vector3 movePlayer = Vector3.zero;
+
+        if (Input.GetKeyDown(KeyCode.LeftArrow) && movementTimer == 0) {
+            direction = Direction.west;
+            movePlayer.x = -speed;
+            movementTimer += movementCooldown;
+        }
+        if (Input.GetKeyDown(KeyCode.RightArrow) && movementTimer == 0) {
+            direction = Direction.east;
+            movePlayer.x = speed;
+            movementTimer += movementCooldown;
+        }
+        if (Input.GetKeyDown(KeyCode.UpArrow) && movementTimer == 0) {
+            direction = Direction.north;
+            movePlayer.y = speed;
+            movementTimer += movementCooldown;
+        }
+        if (Input.GetKeyDown(KeyCode.DownArrow) && movementTimer == 0) {
+            direction = Direction.south;
+            movePlayer.y = -speed;
+            movementTimer += movementCooldown;
+        }
+
+        transform.position += movePlayer;
+    }
+    void UpdateTimer() {
+        if (movementTimer > 0) {
+            movementTimer--;
+        }
+    }
+}
