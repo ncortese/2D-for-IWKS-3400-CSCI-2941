2119581f8dee184ebbe065e408bded82cc0f813c
mibikin <mibikin@gmail.com>
2018-02-12 12:09:49 -0700

    Updated player movment
    Added health and xp controllers to movement class. Need to add player collision detection

--- Scripts/playerMovement.cs
+++ Scripts/playerMovement.cs
@@ -7,17 +7,23 @@ public class playerMovement : MonoBehaviour {
     private enum Direction { north = 0, south = 1, east = 2, west = 3 }
 
     // Public values can be adjusted in unity
-    public int speed = 4;
-    public int movementCooldown = 30;
+    public int speed = 1;
+    public int movementCooldown = 3;
     public bool allowHoldToMove = true;
+	public int maxHealth = 10;
+	public float xpGrowth = 0.15f;
+	public float xpToLevel = 10;
 
     // Private values for behind the scenes work
     private int movementTimer = 0;
+	private int health;
+	private float xp = 0;
+	private int level = 1;
     private Direction direction;
 
 	// Use this for initialization
 	void Start () {
-		
+		health = maxHealth;
 	}
 	
 	// Update is called once per frame
@@ -28,6 +34,7 @@ public class playerMovement : MonoBehaviour {
         else {
             ControlPlayerTap();
         }
+		XPController();
         UpdateTimer();
 	}
 
@@ -58,6 +65,7 @@ public class playerMovement : MonoBehaviour {
 
         transform.position += movePlayer;
     }
+
     void ControlPlayerTap() {
         // Used for moving the player
         Vector3 movePlayer = Vector3.zero;
@@ -85,9 +93,26 @@ public class playerMovement : MonoBehaviour {
 
         transform.position += movePlayer;
     }
+
     void UpdateTimer() {
         if (movementTimer > 0) {
             movementTimer--;
         }
     }
+
+	void XPController() {
+		if (xp == xpToLevel) {
+			xp = 0;
+			xpToLevel *= xpGrowth;
+			level++;
+			maxHealth += 5;
+			health = maxHealth;
+		}
+		else if (xp > xpToLevel) {
+			xp -= xpToLevel;
+			level++;
+			maxHealth += 5;
+			health = maxHealth;
+		}
+	}
 }
